9b5f7676acb065e1b756701ce07e4b07f53a6e6e
Gael Dewas <gael.dewas@gmail.com>
2024-06-03 01:03:38 +0200

    fetch POST défaillant

---
+++ FrontEnd/modal.js
@@ -0,0 +1,270 @@
+//Création de la fenêtre modale
+
+export { createModal, setupContentGalleryModal };
+import { storedData } from './script.js';
+import { fetchGeneric } from './script.js';
+
+function createModal() {
+  const modal = document.createElement('div');
+  modal.id = 'modalId';
+  modal.className = 'modal';
+
+  const modalContent = document.createElement('div');
+  modalContent.className = 'modal-content';
+
+  const closeBtn = createCloseButton();
+  modalContent.appendChild(closeBtn);
+  closeBtn.addEventListener('click', () => {
+    deleteModal('modalId');
+  });
+
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
+
+  return modal;
+}
+
+function createCloseButton() {
+  const closeBtn = document.createElement('span');
+  closeBtn.className = 'close';
+  closeBtn.textContent = '×';
+
+  return closeBtn;
+}
+
+function deleteModal(modalId) {
+  const modalToRemove = document.getElementById(modalId);
+  modalToRemove.remove();
+}
+
+function setupContentGalleryModal(data) {
+  const modal = document.getElementById('modalId');
+  const modalContent = modal.querySelector('.modal-content');
+
+  const galleryContainer = document.createElement('div');
+  galleryContainer.className = 'gallery-container';
+  modalContent.appendChild(galleryContainer);
+
+  const title = document.createElement('h2');
+  title.textContent = 'Galerie Photo';
+  galleryContainer.appendChild(title);
+
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'image-container';
+  displayImagesInModal(data, imageContainer);
+  galleryContainer.appendChild(imageContainer);
+
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Ajouter une photo';
+  addButton.className = 'btn btn-primary';
+  addButton.id = 'addPhotoButton';
+  galleryContainer.appendChild(addButton);
+
+  addButton.addEventListener('click', () => {
+    modalContent.removeChild(galleryContainer);
+    setupContentAddPhoto();
+  });
+}
+
+function displayImagesInModal(data, container) {
+  data.forEach((item) => {
+    const imgWrapper = document.createElement('div');
+    imgWrapper.className = 'img-wrapper';
+    imgWrapper.setAttribute('data-id', item.id);
+
+    const img = document.createElement('img');
+    img.src = item.imageUrl;
+    img.alt = item.title;
+    img.className = 'gallery-img';
+
+    const deleteIcon = document.createElement('i');
+    deleteIcon.className = 'fa-solid fa-trash-can delete-icon';
+    deleteIcon.onclick = () => deleteProject(item.id);
+
+    imgWrapper.appendChild(img);
+    imgWrapper.appendChild(deleteIcon);
+    container.appendChild(imgWrapper);
+  });
+}
+
+async function deleteProject(id) {
+  const headers = {
+    Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+  };
+  await fetchGeneric(id, 'DELETE', headers);
+
+  //TODO: Rajouter message d'erreur et gestion du statut
+}
+
+let uploadedImgUrl = null;
+
+function setupContentAddPhoto() {
+  const modal = document.getElementById('modalId');
+  const modalContent = modal.querySelector('.modal-content');
+
+  const mainContainer = document.createElement('div');
+  mainContainer.className = 'main-container';
+  modalContent.appendChild(mainContainer);
+
+  const returnBtn = document.createElement('span');
+  returnBtn.className = 'return-btn fa-solid fa-arrow-left';
+  mainContainer.appendChild(returnBtn);
+  returnBtn.addEventListener('click', () => {
+    modalContent.removeChild(mainContainer);
+    setupContentGalleryModal(storedData);
+  });
+
+  const title = document.createElement('h2');
+  title.textContent = 'Ajout Photo';
+  mainContainer.appendChild(title);
+
+  const formContainer = document.createElement('div');
+  formContainer.className = 'form-container';
+  mainContainer.appendChild(formContainer);
+
+  const form = document.createElement('form');
+  form.id = 'addPhotoForm';
+  form.action = '#';
+  form.method = 'post';
+  formContainer.appendChild(form);
+
+  // Input photo
+  const uploadContainer = document.createElement('div');
+  uploadContainer.className = 'upload-container';
+  const icon = document.createElement('i');
+  icon.className = 'upload-icon fa-regular fa-image';
+
+  const inputFile = document.createElement('input');
+  inputFile.type = 'file';
+  inputFile.id = 'inputFile';
+  inputFile.accept = 'image/png, image/jpeg';
+  const uploadLabel = document.createElement('label');
+  uploadLabel.htmlFor = 'inputFile';
+  uploadLabel.id = 'uploadBtn';
+
+  const labelContent = document.createElement('span');
+  labelContent.textContent = '+ Ajouter photo';
+  labelContent.className = 'btn';
+  uploadLabel.appendChild(labelContent);
+
+  const uploadText = document.createElement('span');
+  uploadText.textContent = 'jpg, png : 4mo max';
+  uploadText.className = 'upload-text';
+
+  uploadContainer.appendChild(icon);
+  uploadContainer.appendChild(uploadLabel);
+  uploadContainer.appendChild(inputFile);
+  uploadContainer.appendChild(uploadText);
+  form.appendChild(uploadContainer);
+
+  // Input titre
+  const titleLabel = document.createElement('label');
+  titleLabel.textContent = 'Titre';
+  titleLabel.id = 'titleLabel';
+  titleLabel.htmlFor = 'photoTitle';
+  const titleInput = document.createElement('input');
+  titleInput.type = 'text';
+  titleInput.id = 'photoTitle';
+  titleInput.name = 'photoTitle';
+  form.appendChild(titleLabel);
+  form.appendChild(titleInput);
+
+  // Input Catégorie
+  const categoryLabel = document.createElement('label');
+  categoryLabel.textContent = 'Catégorie';
+  categoryLabel.id = 'categoryLabel';
+  categoryLabel.htmlFor = 'photoCategory';
+
+  const categorySelect = document.createElement('select');
+  categorySelect.id = 'photoCategory';
+  categorySelect.name = 'photoCategory';
+
+  const categories = ['Objets', 'Appartements', 'Hôtels & Restaurants'];
+  categories.forEach((category) => {
+    const option = document.createElement('option');
+    option.textContent = category;
+    categorySelect.appendChild(option);
+  });
+
+  form.appendChild(categoryLabel);
+  form.appendChild(categorySelect);
+
+  // Bouton Valider
+  const submitDiv = document.createElement('div');
+  submitDiv.className = 'submit-div';
+  form.appendChild(submitDiv);
+
+  const submitButton = document.createElement('input');
+  submitButton.type = 'submit';
+  submitButton.id = 'btnValider';
+  submitButton.value = 'Valider';
+  submitButton.className = 'btn btn-valider';
+  submitDiv.appendChild(submitButton);
+
+  replaceIconByUpload('inputFile');
+  submitForm(form);
+}
+
+function replaceIconByUpload(input) {
+  document.getElementById(input).addEventListener('change', function (event) {
+    event.preventDefault;
+    const file = event.target.files[0];
+    if (!file) return;
+    const maxSize = 4 * 1048576;
+
+    if (file.size > maxSize) {
+      alert('Le fichier doit faire 4 Mo ou moins.');
+      return;
+    }
+
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      const uploadedImg = document.createElement('img');
+      uploadedImg.src = e.target.result;
+      uploadedImg.className = 'uploaded-img';
+
+      uploadedImgUrl = e.target.result;
+
+      const container = document.querySelector('.upload-container');
+      const icon = document.querySelector('.fa-image');
+      container.replaceChild(uploadedImg, icon);
+    };
+    reader.readAsDataURL(file);
+  });
+}
+
+function submitForm(form) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    ajoutProjet();
+  });
+}
+
+function ajoutProjet() {
+  const image = uploadedImgUrl;
+  const titre = document.getElementById('photoTitle').value;
+  const categorySelect = document.getElementById('photoCategory').value;
+
+  const formData = new FormData();
+  formData.append('titre', titre);
+  formData.append('imageUrl', image);
+  formData.append('categoryId', categorySelect);
+  console.log(formData);
+
+  const array = Array.from(formData.entries());
+
+  console.log(array);
+  // Array.from(formData.entries()).forEach(([key, value]) => {
+  //   console.log(key, value);
+  // });
+
+  const body = JSON.stringify(array);
+  console.log(body);
+
+  const headers = {
+    Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+    'Content-Type': 'application/json',
+  };
+
+  fetchGeneric(' ', 'POST', headers, body);
+}
---
+++ FrontEnd/script.js
@@ -0,0 +1,177 @@
+let storedData = null;
+let url = 'http://localhost:5678/api/works/';
+
+// Fonction asynchrone pour récupérer les données de l'API
+
+// async function fetchData() {
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Erreur HTTP ! statut : ${response.status}`);
+//     }
+//     return await response.json();
+//   } catch (error) {
+//     console.error('Erreur lors de la récupération des données', error);
+//   }
+// }
+
+export async function fetchGeneric(endUrl, method1, headers1, body1) {
+  let url = 'http://localhost:5678/api/works/' + endUrl;
+
+  const config = {
+    method: method1,
+    headers: new Headers(headers1),
+    body: body1,
+  };
+
+  try {
+    const response = await fetch(url, config);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(
+        `HTTP error! status: ${response.status}, Message: ${data.message}`
+      );
+    }
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
+}
+
+// Fonction principale pour lancer l'application
+async function main() {
+  try {
+    storedData = await fetchGeneric(' ');
+    if (storedData) {
+      displayProjects(storedData);
+      console.log(storedData);
+    }
+  } catch (error) {
+    console.error('Échec du chargement des projets :', error);
+    showErrorMessage(
+      'Échec du chargement des projets. Veuillez réessayer plus tard.'
+    );
+  }
+}
+
+// Affichage des projets dans la galerie
+function displayProjects(data) {
+  const displayedProjectIds = new Set();
+  data.forEach((item) => {
+    if (!displayedProjectIds.has(item.id)) {
+      const project = makeProject(item);
+      appendProjectToGallery(project);
+      displayedProjectIds.add(item.id);
+    }
+  });
+}
+
+// Création d'un élément de projet pour l'affichage
+function makeProject(item) {
+  const { categoryId, title, imageUrl } = item;
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const figCaption = document.createElement('figcaption');
+
+  img.src = imageUrl;
+  img.alt = title;
+  figCaption.textContent = title;
+  figure.className = categoryId;
+  figure.append(img, figCaption);
+
+  return figure;
+}
+
+// Ajout du projet à la galerie
+function appendProjectToGallery(figure) {
+  const gallery = document.querySelector('.gallery');
+  gallery.appendChild(figure);
+}
+
+// Configuration des boutons de filtre
+function setupFilterButtons() {
+  const portfolioSection = document.getElementById('portfolio');
+  const gallery = portfolioSection.querySelector('.gallery');
+  const filterMenu = document.createElement('div');
+  filterMenu.className = 'filter-menu';
+  portfolioSection.insertBefore(filterMenu, gallery);
+
+  const filterButtons = setButtonParams();
+  filterButtons.forEach((itemButton) => {
+    const button = createButton(itemButton.id, itemButton.text);
+    button.addEventListener('click', () =>
+      handleButtonClick(button, itemButton)
+    );
+    filterMenu.appendChild(button);
+  });
+}
+
+// Création d'un bouton
+function createButton(id, text) {
+  const button = document.createElement('button');
+  button.id = id;
+  button.textContent = text;
+  button.className = 'btn btn-select';
+  return button;
+}
+
+// Définition des paramètres des boutons
+function setButtonParams() {
+  return [
+    { id: 'Tous', text: 'Tous', categoryId: null },
+    { id: 'Objets', text: 'Objets', categoryId: 1 },
+    { id: 'Appartements', text: 'Appartements', categoryId: 2 },
+    { id: 'Hotels', text: 'Hotels & Restaurants', categoryId: 3 },
+  ];
+}
+
+// Gestion du clic sur les boutons
+function handleButtonClick(button, itemButton) {
+  resetButtonStyle();
+  button.classList.add('active-button');
+  clearGallery();
+  const filteredData = itemButton.categoryId
+    ? filterData(storedData, itemButton.categoryId)
+    : storedData;
+  displayProjects(filteredData);
+}
+
+// Réinitialisation du style des boutons
+function resetButtonStyle() {
+  document.querySelectorAll('.btn-select').forEach((button) => {
+    button.className = 'btn btn-select';
+  });
+}
+
+// Nettoyage de la galerie
+function clearGallery() {
+  document.querySelectorAll('figure').forEach((figure) => figure.remove());
+}
+
+// Filtrage des données selon la catégorie
+function filterData(data, categoryId) {
+  return categoryId === null
+    ? data
+    : data.filter((item) => item.categoryId === categoryId);
+}
+
+//Rediriger vers la section Login
+
+const loginButton = document.getElementById('login-button');
+loginButton.addEventListener('click', function () {
+  window.location.href = './login.html';
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const loginButton = document.getElementById('login-button');
+  if (window.location.pathname === '/FrontEnd/login.html') {
+    loginButton.style.fontWeight = 'bold';
+  }
+});
+
+// Initialisation de l'application
+setupFilterButtons();
+main();
+
+export { storedData };
